6d682b5851da9bb4f15a1a8e9a14b8eef3b2b3a2
Francisco Igor <franciscoigor@gmail.com>
2018-08-07 09:32:43 -0700

    updated app icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="./app_icon.png" type="image/png" rel="shortcut icon" >
 
         <!-- Styles -->
         <style>
