6a3b8127b3773936b56ac6bc804361307c6dc517
Francisco Igor <franciscoigor@gmail.com>
2018-10-06 21:01:44 -0700

    Added layout and view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<form class="login">
+<div class="login-user">
+    <label for="loginUser">User</label>
+    <input id="loginUser" name="user">
+</div>
+<div class="login-password">
+    <label for="loginpassword" >Password</label>
+    <input id="loginPassword" name="password" type="password">
+</div>
+<div class="login-buttons">
+    <button class="login-button">
+        Login
+    </button>
+</div>
+</form>
